192ede03e199557b7c7078e23266c2b4db48d6ec
a_daud <adam.daudrich@pm.me>
2024-10-21 10:01:23 -0400

    Update sp500.py

--- sp500.py
+++ sp500.py
@@ -6,22 +6,22 @@ SP_500 = []
 interest = []
 
 #isolate desired date range (lines 6:18)
-for line in lines[6:18]:
-    # variable 'columns' is actually the entire row.
-    # the index gives you the item
-    columns = line.strip().split(',')
-    SP_500.append(columns[1])
-    interest.append(columns[5])
 
-# make a list called 'float values' which translates
-# all numbers in the SP_500 list to a float
+for line in lines[6:18]:
+    rows = line.strip().split(',')
+    SP_500.append(rows[1])
+    interest.append(rows[5])
+
+# make a list called 'float values' which translates all numbers in the SP_500 list to a float
+
 float_values = [float(value) for value in SP_500]
 SP_sum = sum(float_values)
 SP_count = len(float_values)
 mean_SP = SP_sum / SP_count
 print('The average SP 500 for the date range is: ', mean_SP)
 
-# same thing, translate interest to a new list of floats                  
+# translate interest to a new list of floats
+
 interest_values = [float(v) for v in interest]
 max_interest = max(interest_values)
 print('The max interest value for the date range is: ', max_interest)
